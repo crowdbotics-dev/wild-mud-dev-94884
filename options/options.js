d03d94ae6334b9d017f69a3e0613d4e6d56b3b80
Crowdbotics <engineering@crowdbotics.com>
2023-06-26 11:39:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wild-mud-dev-94884",
+  url: "https://wild-mud-dev-94884.botics.co",
+  api: "https://wild-mud-dev-94884.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
